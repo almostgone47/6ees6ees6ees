4db1ce57205e27bd2d7b0ea3e1112b992c0a9a6a
Jeremiah <almostgone47@hotmail.com>
2019-12-25 16:33:47 -0800

    made some bees

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,8 @@
-class Bee {
-  // TODO..
+class Bee extends Grub {
+  constructor(age, color, food, job) {
+    super(food)
+    this.color = 'yellow';
+    this.age = 5;
+    this.job = 'Keep on growing';
+  }
 };
